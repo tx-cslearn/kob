2d375c76945a8f09570437186980feded9b4b04d
txtx-cloud <1092145182@qq.com>
2022-07-13 21:21:49 +0800

    创建游戏背景

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,41 +1,27 @@
 <template>
-  <div>
-    <div>Bot昵称：{{ bot_name }}</div>
-    <div>Bot战力：{{ bot_rating }}</div>
-  </div>
-  <router-view></router-view>
+  <NavBar />
+  <router-view />
 </template>
 
 <script>
-import { ref } from 'vue';
-import $ from 'jquery';
+//import { ref } from 'vue';
+//import $ from 'jquery';
+import NavBar from './components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
 
 export default {
   name: "App",
-  setup: () => {
-    let bot_name = ref("");
-    let bot_rating = ref("");
-
-    $.ajax({
-      url: "http://localhost:9999/pk/getbotinfo/",
-      type: "get",
-      success: resp => {
-        bot_name.value = resp.name;
-        bot_rating.value = resp.rating;
-      }
-    });
-
-    return {
-      bot_name,
-      bot_rating
-    }
+  components: {
+    NavBar,
   }
 }
+
 </script>
 
 <style>
 body {
-  background-image: url("@/assets/1_59b8f162fd-background.png");
-  background-size: cover;
+  background-image: url("@/assets/images/1_59b8f162fd-background.png");
+  background-size: 100%;
 }
 </style>
---
+++ web/src/views/error/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+    <ContentField>
+        404 not found
+    </ContentField>
+</template>
+
+<script>
+import ContentField from '../../components/ContentField.vue'
+
+export default {
+    name: "NotFound",
+    components: {
+        ContentField
+    }
+}
+</script>
+
+<style scoped>
+</style>
