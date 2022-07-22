9457d3178840415899b0e1a53e3e33308475bd05
txtx-cloud <1092145182@qq.com>
2022-07-22 14:07:06 +0800

    配置MYsql与注册登录

--- kob/src/test/java/com/kob/backend/KobApplicationTests.java
+++ kob/src/test/java/com/kob/backend/KobApplicationTests.java
@@ -2,12 +2,19 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class KobApplicationTests {
 
     @Test
     void contextLoads() {
+        PasswordEncoder ps = new BCryptPasswordEncoder();
+        System.out.println(ps.encode("tx"));
+        System.out.println(ps.encode("syj"));
+        System.out.println(ps.encode("czd"));
+
     }
 
 }
