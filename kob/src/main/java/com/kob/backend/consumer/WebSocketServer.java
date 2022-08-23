3c2b4e0a5e9c428b4fb668038eb62f7170aba0df
txtx-cloud <1092145182@qq.com>
2022-08-23 16:59:24 +0800

    匹配系统（中）

--- kob/src/main/java/com/kob/backend/consumer/WebSocketServer.java
+++ kob/src/main/java/com/kob/backend/consumer/WebSocketServer.java
@@ -3,6 +3,7 @@ package com.kob.backend.consumer;
 import com.alibaba.fastjson.JSONObject;
 import com.kob.backend.consumer.utils.Game;
 import com.kob.backend.consumer.utils.JwtAuthentication;
+import com.kob.backend.mapper.RecordMapper;
 import com.kob.backend.mapper.UserMapper;
 import com.kob.backend.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import javax.xml.ws.WebEndpoint;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,16 +22,24 @@ import java.util.concurrent.CopyOnWriteArraySet;
 @ServerEndpoint("/websocket/{token}")  // 注意不要以'/'结尾
 public class WebSocketServer {
 
-    final private static ConcurrentHashMap<Integer,WebSocketServer> users = new ConcurrentHashMap<>();
+    final public static ConcurrentHashMap<Integer,WebSocketServer> users = new ConcurrentHashMap<>();
 
     final private static CopyOnWriteArraySet<User> matchpool = new CopyOnWriteArraySet<>();
     private User user;
     private Session session = null;
 
     private static UserMapper userMapper;
+    public static RecordMapper recordMapper;
+    private Game game;
+
     @Autowired
     public void setUserMapper(UserMapper userMapper){
         WebSocketServer.userMapper=userMapper;
+    }
+
+    @Autowired
+    public void setRecordMapper(RecordMapper recordMapper){
+        WebSocketServer.recordMapper = recordMapper;
     }
     @OnOpen
     public void onOpen(Session session, @PathParam("token") String token) throws IOException {
@@ -57,6 +67,13 @@ public class WebSocketServer {
         }
     }
 
+    private void move(int direction){
+        if(game.getPlayerA().getId().equals((user.getId()))){
+            game.setNextStepA(direction);
+        }else if(game.getPlayerB().getId().equals(user.getId())){
+            game.setNextStepB(direction);
+        }
+    }
     @OnMessage
     public void onMessage(String message, Session session) {
         // 从Client接收消息
@@ -67,15 +84,14 @@ public class WebSocketServer {
             startMatching();
         }else if ("stop-matching".equals(event)){
             stopMatching();
+        }else if("move".equals(event)){
+            move(data.getInteger("direction"));
         }
-
     }
 
     private void stopMatching() {
         System.out.println("stop matching");
         matchpool.remove(this.user);
-
-
     }
 
     private void startMatching() {
@@ -88,26 +104,40 @@ public class WebSocketServer {
             matchpool.remove(a);
             matchpool.remove(b);
 
-            Game game = new Game(13,14,20);
+            Game game = new Game(13,14,20,a.getId(),b.getId());
             game.createMap();
+
+            users.get(a.getId()).game = game;
+            users.get(b.getId()).game = game;
+
+            game.start();
+            JSONObject respGame = new JSONObject();
+            respGame.put("a_id",game.getPlayerA().getId());
+            respGame.put("a_sx",game.getPlayerA().getSx());
+            respGame.put("a_sy",game.getPlayerA().getSy());
+            respGame.put("b_id",game.getPlayerB().getId());
+            respGame.put("b_sx",game.getPlayerB().getSx());
+            respGame.put("b_sy",game.getPlayerB().getSy());
+            respGame.put("map",game.getG());
+
 
             JSONObject respA = new JSONObject();
             respA.put("event","start-matching");
             respA.put("opponent_username",b.getUsername());
             respA.put("opponent_photo",b.getPhoto());
-            respA.put("gamemap",game.getG());
+            respA.put("game",respGame);
             users.get(a.getId()).sendMessage(respA.toJSONString());
 
             JSONObject respB = new JSONObject();
             respB.put("event","start-matching");
             respB.put("opponent_username",a.getUsername());
             respB.put("opponent_photo",a.getPhoto());
-            respB.put("gamemap",game.getG());
+            respB.put("game",respGame);
             users.get(b.getId()).sendMessage(respB.toJSONString());
         }
     }
 
-    private void sendMessage(String message ) {
+    public void sendMessage(String message ) {
         synchronized (this.session){
             try {
                 this.session.getBasicRemote().sendText(message);
--- web/src/components/GameMap.vue
+++ web/src/components/GameMap.vue
@@ -15,7 +15,12 @@ export default {
         let parent = ref(null);
         let canvas = ref(null);
 
-        onMounted(() => { new GameMap(canvas.value.getContext('2d'), parent.value, store) });
+        onMounted(() => {
+            store.commit(
+                "updateGameObject",
+                new GameMap(canvas.value.getContext('2d'), parent.value, store),
+            );
+        });
 
         return { parent, canvas }
     }
