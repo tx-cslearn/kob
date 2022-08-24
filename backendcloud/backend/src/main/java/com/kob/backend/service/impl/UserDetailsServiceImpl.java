415a4422767f045e37ad5c9f80a482e2fc81daa2
txtx-cloud <1092145182@qq.com>
2022-08-24 20:43:02 +0800

    匹配（下）

---
+++ backendcloud/backend/src/main/java/com/kob/backend/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.kob.backend.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.kob.backend.mapper.UserMapper;
+import com.kob.backend.pojo.User;
+import com.kob.backend.service.impl.utils.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserMapper userMapper;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        QueryWrapper<User> qc = new QueryWrapper<>();
+        qc.eq("username",username);
+        User user = userMapper.selectOne(qc);
+        if(user==null){
+            throw new RuntimeException("user不存在");
+        }
+        return new UserDetailsImpl(user);
+    }
+}
